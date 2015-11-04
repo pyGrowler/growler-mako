eaa0ee158f51c25d51eadad8be484ffee16ba580
Andrew Kubera <andrewkubera@gmail.com>
2015-11-03 19:15:48 -0500

    Added copy of Growler@0.6.1's default MakoRenderer

--- growler_mako/mako_renderer.py
+++ growler_mako/mako_renderer.py
@@ -2,11 +2,27 @@
 # growler_mako/mako_renderer.py
 #
 
+import logging
+from mako.template import Template
+
 
 class MakoRenderer:
     """
     Renderer middleware for mako templates
     """
 
+    DEFAULT_FILE_EXTENSIONS = [
+        '.mako',
+    ]
+
     def __init__(self):
-        pass
+        self._render = Template
+
+        self.log = logging.getLogger(__name__)
+        self.log.info("{} Constructed MakoRenderer".format(id(self)))
+
+    def __call__(self, filename, res):
+        self.log.info("%d -> %s" % (id(self), filename))
+        tmpl = self._render(filename=filename)
+        html = tmpl.render()
+        return html
