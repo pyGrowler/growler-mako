6e33c7ceb6d97ad2ab06bc89cf516bb8789e9583
Andrew Kubera <andrewkubera@gmail.com>
2016-05-25 04:00:58 -0400

    __meta__: Changed version string to version_info tuple

--- growler_mako/__meta__.py
+++ growler_mako/__meta__.py
@@ -5,7 +5,9 @@
 Metadata file.
 """
 
-version = '0.0.1'
+version_info = (0, 0, 1)
+version = '.'.join(map(str, version_info))
+
 date = 'Nov 03, 2015'
 author = 'Andrew Kubera'
 author_email = 'andrewkubera@gmail.com'
